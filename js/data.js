b875cc514deefde6599743d985a534bdebcea58c
rubenaguilardev <ragui56@wgu.edu>
2025-04-05 00:21:42 -0700

    Add javascript functionality

--- js/data.js
+++ js/data.js
@@ -4,20 +4,20 @@ export const menuArray = [
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "pizza.png"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        emoji: "🍔",
+        emoji: "burger.png",
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
-        emoji: "🍺",
+        emoji: "beer.png",
         id: 2
     }
 ]
--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-import { menuArray } from './js/data.js'
+import { menuArray } from '../js/data.js'
 
-console.log(menuArray)
+const main = document.getElementById('main-section')
+
+
+menuArray.forEach(menuObject => {
+    main.innerHTML += `
+        <section class="foodItem">
+            <div class="section-left">
+                <img src="images/${menuObject.emoji}" alt="pizza">
+                <div class="foodDetails">
+                    <h3>${menuObject.name}</h3>
+                    <p>${menuObject.ingredients}</p>
+                    <span>$${menuObject.price}</span>
+                </div>
+            </div>
+            <div class="section-right">
+                <img class="add-button" src="images/add-button.png" alt="">
+            </div>            
+        </section> `
+})
+
