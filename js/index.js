fbe5b12b46d371ced58939bb8ca50709d0e50302
rubenaguilardev <ragui56@wgu.edu>
2025-04-05 01:56:37 -0700

    Add order-pricing sections

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 import { menuArray } from '../js/data.js'
 
-const main = document.getElementById('main-section')
+const main = document.getElementById('foodItem')
 
 
 menuArray.forEach(menuObject => {
     main.innerHTML += `
         <section class="foodItem">
-            <div class="section-left">
-                <img src="images/${menuObject.emoji}" alt="pizza">
+            <div class="foodItem-left">
+                <img class="food-item-image" src="images/${menuObject.emoji}" alt="pizza">
                 <div class="foodDetails">
                     <h3>${menuObject.name}</h3>
                     <p>${menuObject.ingredients}</p>
                     <span>$${menuObject.price}</span>
                 </div>
             </div>
-            <div class="section-right">
+            <div class="foodItem-right">
                 <img class="add-button" src="images/add-button.png" alt="">
             </div>            
         </section> `
