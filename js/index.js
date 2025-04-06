f6b60bf9fa22bdab2abc465542af24ad9b5498f9
rubenaguilardev <ragui56@wgu.edu>
2025-04-06 02:41:15 -0700

    Updates

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 import { menuArray } from '../js/data.js'
 
 const main = document.getElementById('foodItem')
+const orderSection = document.getElementById('order-section')
+const price = document.getElementById('price-span')
+const checkout = document.getElementById('checkout-btn')
+
+let newPrice = 0
 
 
 menuArray.forEach(menuObject => {
+
     main.innerHTML += `
         <section class="foodItem">
             <div class="foodItem-left">
-                <img class="food-item-image" src="images/${menuObject.emoji}" alt="pizza">
+                <img class="food-item-image" src="images/${menuObject.emoji}" alt="${menuObject.name}">
                 <div class="foodDetails">
                     <h3>${menuObject.name}</h3>
                     <p>${menuObject.ingredients}</p>
@@ -15,8 +21,40 @@ menuArray.forEach(menuObject => {
                 </div>
             </div>
             <div class="foodItem-right">
-                <img class="add-button" src="images/add-button.png" alt="">
+                <img id="${menuObject.id}" class="add-button" src="images/add-button.png" alt="button">
             </div>            
         </section> `
 })
+
+document.addEventListener('click', handleOnClick)
+
+
+function handleOnClick(e) {  
+
+    const id = e.target.id
+    
+    if (id) {
+        document.getElementById('order').style.display = 'block'      
+        orderSection.innerHTML += `
+            <div class="order-info" data-id="${menuArray[id].id}">
+                <div class="order-left">
+                    <h3 class="order-item">${menuArray[id].name}</h3>
+                    <span class="remove-span" data-remove="${menuArray[id].id}">remove</span>
+                </div>
+                <span class="order-span">$${menuArray[id].price}</span>
+            </div>`
+        priceIncrease(id)
+        
+    }
+}
+
+
+function priceIncrease(id) {
+    newPrice += menuArray[id].price
+    price.textContent = `$${newPrice}`
+}
+
+
+
+
 
