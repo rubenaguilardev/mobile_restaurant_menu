b9733733fb86842a5efb068f9a844a7528411929
rubenaguilardev <ragui56@wgu.edu>
2025-04-10 04:11:59 -0700

    completed

--- js/data.js
+++ js/data.js
@@ -4,20 +4,23 @@ export const menuArray = [
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         price: 14,
         emoji: "pizza.png",
-        id: 0
+        id: 0,
+        quantity: 0
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         emoji: "burger.png",
-        id: 1
+        id: 1,
+        quantity: 0
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "beer.png",
-        id: 2
+        id: 2,
+        quantity: 0
     }
 ]
--- js/index.js
+++ js/index.js
@@ -4,8 +4,13 @@ const main = document.getElementById('foodItem')
 const orderSection = document.getElementById('order-section')
 const price = document.getElementById('price-span')
 const checkout = document.getElementById('checkout-btn')
+const paymentPopup = document.querySelector('.payment-popup')
+const paymentForm = document.querySelector('.payment-form')
+
+
 
 let newPrice = 0
+const foodItems = []
 
 
 menuArray.forEach(menuObject => {
@@ -31,27 +36,144 @@ document.addEventListener('click', handleOnClick)
 
 function handleOnClick(e) {  
 
-    const id = e.target.id
+    if (e.target.id === 'checkout-btn') {
+
+        completeOrder()
+    }
+
+    else if (e.target.id === 'close-btn') {
+
+        backToOrder()
+    }
     
-    if (id) {
-        document.getElementById('order').style.display = 'block'      
+    else if (e.target.id) {  
+        
+        addItem(e.target.id)
+        
+        document.getElementById('order').style.display = 'block'
+        document.getElementById(`item-${e.target.id}`).style.display = 'flex'
+    }
+
+    else if (e.target.dataset.remove) {
+
+        removeOnClick(e.target.dataset.remove)    
+    }
+   
+}
+
+paymentForm.addEventListener('submit', checkoutOrder)
+
+
+
+function addItem(id) {
+    document.querySelector('.order-placed').style.display = 'none'
+    menuArray[id].quantity++
+    if (foodItems.includes(id)) {
+        quantityIncrease(id)
+    }
+    else {
         orderSection.innerHTML += `
-            <div class="order-info" data-id="${menuArray[id].id}">
-                <div class="order-left">
-                    <h3 class="order-item">${menuArray[id].name}</h3>
+        <div class="order-info" id="item-${menuArray[id].id}">
+            <div class="order-left">
+                <h3 class="order-item" {data-item="${menuArray[id].id}">${menuArray[id].name}</h3>
+                <div class="order-quantity">
+                    <h4 id="quantityIncease${id}">Quantity: ${menuArray[id].quantity}</h4>
                     <span class="remove-span" data-remove="${menuArray[id].id}">remove</span>
                 </div>
-                <span class="order-span">$${menuArray[id].price}</span>
-            </div>`
-        priceIncrease(id)
-        
+            </div>
+            <span class="order-span">$${menuArray[id].price}</span>
+        </div>`
+        quantityIncrease(id)
+        foodItems.push(id)
     }
+    
+    
+    
+    priceIncrease(id)
 }
+
+
+
+
+
+function removeOnClick(id) {
+    if (menuArray[id].quantity > 0) {
+        priceDecrease(id)
+        quantityDecrease(id)
+        if (menuArray[id].quantity === 0) {
+            document.getElementById(`item-${id}`).style.display = 'none'
+        }
+    } 
+
+    if (newPrice === 0) {
+        document.querySelector('#order').style.display = 'none'
+    }  
+}
+    
+
 
 
 function priceIncrease(id) {
     newPrice += menuArray[id].price
     price.textContent = `$${newPrice}`
+}
+
+
+function priceDecrease(id) {
+    newPrice -= menuArray[id].price
+    price.textContent = `$${newPrice}`
+}
+
+
+function quantityIncrease(id) {
+    const quantity = document.getElementById(`quantityIncease${id}`)
+    quantity.textContent = `Quantity: ${menuArray[id].quantity}`
+}
+
+function quantityDecrease(id) {
+    const quantity = document.getElementById(`quantityIncease${id}`)
+    menuArray[id].quantity--
+    quantity.textContent = `Quantity: ${menuArray[id].quantity}`
+    
+}
+
+
+function completeOrder() {
+    if (newPrice > 0) {
+        paymentPopup.style.display = 'block'
+        main.style.backgroundImage = 'linear-gradient(gray, white)'  
+    }
+   
+}
+
+
+function backToOrder() {
+    paymentPopup.style.display = 'none'
+    main.style.backgroundImage = 'none'
+    checkout.style.backgroundColor = '#16DB99'
+}
+
+
+
+function checkoutOrder(e) {
+    const ids = [0, 1, 2]
+    e.preventDefault()
+    const paymentFormData = new FormData(paymentForm)
+    const name = paymentFormData.get('full-name')
+    document.getElementById('order').style.display = 'none'
+    main.style.backgroundImage = 'none'
+    paymentPopup.style.display = 'none'
+    document.getElementById('confirmation-p').textContent = `Thanks, ${name}! Your order is on its way`
+    document.querySelector('.order-placed').style.display = 'flex'
+    menuArray.forEach(item => {
+        item.quantity = 0
+        
+    })
+    ids.forEach(id => {
+        document.getElementById(`item-${id}`).style.display = 'none'
+    })
+    newPrice = 0
+    
 }
 
 
